c2683e81ac3f9a5ce04eca3705a7eb19fcdb9580
americanman <snowboardracer7@gmail.com>
2018-08-18 15:59:08 -0400

    Create Calculator class

--- features/step_definition/addition_steps.js
+++ features/step_definition/addition_steps.js
@@ -1,20 +1,15 @@
-   const { Before, Given, When, Then } = require('cucumber')
-
-   Given the numbers 2 and 3
+   const { Before, Given, When, Then } = require('cucumber');
+   const Calculator = require('../../lib/calculator');
 
          Given('the numbers {int} and {int}', function (int, int2) {
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
 
-    When they are added together
-
          When('they are added together', function () {
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
-
-    Then should the result be 5
 
          Then('should the result be {int}', function (int) {
            // Write code here that turns the phrase above into concrete actions
