858c520d36bb4880f27030c67cf964ac74c3f8de
americanman <snowboardracer7@gmail.com>
2018-08-18 17:01:01 -0400

    implement calculator constructor for Given

--- features/step_definition/addition_steps.js
+++ features/step_definition/addition_steps.js
@@ -1,18 +1,19 @@
-   const { Before, Given, When, Then } = require('cucumber');
-   const Calculator = require('../../lib/calculator');
+const { Before, Given, When, Then } = require('cucumber');
+const Calculator = require('../../lib/calculator.js');
 
-         Given('the numbers {int} and {int}', function (int, int2) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+let calculator;
 
-         When('they are added together', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Given('the numbers {int} and {int}', function (x, y, callback) {
+    calculator = new Calculator(x, y);
+    callback();
+});
 
-         Then('should the result be {int}', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+When('they are added together', function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
 
+Then('should the result be {int}', function (int, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
