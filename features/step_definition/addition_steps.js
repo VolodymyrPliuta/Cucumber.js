b19db43e958d10d14813ff8102eb7c871f926e35
americanman <snowboardracer7@gmail.com>
2018-08-18 15:33:26 -0400

    Add missing steps

---
+++ features/step_definition/addition_steps.js
@@ -0,0 +1,21 @@
+   Given the numbers 2 and 3
+
+         Given('the numbers {int} and {int}', function (int, int2) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+    When they are added together
+
+         When('they are added together', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+    Then should the result be 5
+
+         Then('should the result be {int}', function (int) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
